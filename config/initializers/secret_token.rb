039faad677a523b0cb7758c48d34e9cb3aa3b9f8
Elliot Nash <elliot@shopximity.com>
2012-10-01 08:32:37 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Splitdocs::Application.config.secret_token = '28492ca7aa4af95e8c1037ed971d5506f7c0bc8683bbf91d0f1fe92a6ed23575da5cbc1ba19ae09a2c00af7df45254f39ae9f99d3da67a63fa5c32029faeacc9'
